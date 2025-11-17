9b5c4ca424757a80558a4ce99469c136acd132cd
Nirupama Ram <nram@doximity.com>
2025-11-17 09:12:34 -0500

    Add show and edit page

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,6 +1,9 @@
 class MeetingsController < ApplicationController
+  before_action :find_meeting, only: [:show, :edit, :update]
+
   def new
     @meeting = Meeting.new
+    @users = User.all
   end
 
   def create
@@ -9,7 +12,24 @@ class MeetingsController < ApplicationController
     if @meeting.save
       redirect_to root_path, notice: 'Meeting scheduled successfully.'
     else
+      @users = User.all
       render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    @users = User.all
+  end
+
+  def update
+    if @meeting.update(meeting_params)
+      redirect_to meeting_path(@meeting), notice: 'Meeting updated successfully.'
+    else
+      @users = User.all  # needed if your form uses @users
+      render :edit, status: :unprocessable_entity
     end
   end
 
@@ -17,5 +37,9 @@ class MeetingsController < ApplicationController
 
   def meeting_params
     params.require(:meeting).permit(:bottle_bringer_id, :date)
+  end
+
+  def find_meeting
+    @meeting = Meeting.find(params[:id])
   end
 end
